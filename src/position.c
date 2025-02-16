97bbd2ece6522b6546fa2e0c7f534678a1e071bd
Athena Boose <pestpestthechicken@yahoo.com>
2025-02-15 21:53:55 -0800

    added print all pts fn

--- src/position.c
+++ src/position.c
@@ -10,3 +10,10 @@ void print_at_position(Point point, int row, int col) {
     CURSOR_UP(row);
 }
 
+void print_all_points(Point *points, int num_rows, int num_cols) {
+    for (int i = 0; i < num_rows; ++i) {
+        for (int j = 0; j < num_cols; ++j) {
+            print_at_position(points[i * num_cols + j], num_rows, num_cols);
+        }
+    }
+}
--- src/position.h
+++ src/position.h
@@ -2,4 +2,4 @@
 #include <ctty/cursor.h>
 #include "structs.h"
 void print_at_position(Point point, int row, int col);
-
+void print_all_points(Point *points, int num_rows, int num_cols);
