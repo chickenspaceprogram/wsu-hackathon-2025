8e70c96264e8941f92e1efd4d00c78546bdf7516
Athena Boose <pestpestthechicken@yahoo.com>
2025-02-15 21:45:10 -0800

    yeet

--- src/hack_menus.c
+++ src/hack_menus.c
@@ -1,4 +1,5 @@
 #include "hack_menus.h"
+#include <ctty/cursor.h>
 
 
 #ifdef _WIN32
@@ -6,14 +7,17 @@
 
 void print_symbol_menu(void)
 {
+    CURSOR_UP(5);
 	printf("%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c\n", 201, 205, 209, 205, 203, 205, 209, 205, 203, 205, 209, 205, 203, 205, 209, 205, 203, 205, 209, 205, 187);
 	printf("%c%d%c%c%c%d%c%c%c%d%c%c%c%d%c%c%c%d%c%c%c\n", 186, 1, 179, 176, 186, 2, 179, 177, 186, 3, 179, 178, 186, 4, 179, 219, 186, 5, 179, 220, 186);
 	printf("%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c\n", 204, 205, 216, 205, 206, 205, 216, 205, 206, 205, 216, 205, 206, 205, 216, 205, 206, 205, 216, 205, 185);
 	printf("%c%d%c%c%c%d%c%c%c%d%c%c%c%d%c%c%c%c%c%c%c\n", 186, 6, 179, 221, 186, 7, 179, 222, 186, 8, 179, 223, 186, 9, 179, 254, 186, ' ', 179, ' ', 186);
 	printf("%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c\n", 200, 205, 207, 205, 202, 205, 207, 205, 202, 205, 207, 205, 202, 205, 207, 205, 202, 205, 207, 205, 188);
+    CURSOR_DOWN_LINE_START(1);
 }
 void print_color_menu(void)
 {
+    CURSOR_UP(5);
 	printf("%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c\n", 201, 205, 209, 205, 203, 205, 209, 205, 203, 205, 209, 205, 203, 205, 209, 205, 187);
 	printf("%c%d%c", 186, 1, 179);
 	printf("\033[40;37m"); printf("B"); printf("\033[49;39m");//1 black on white
@@ -34,24 +38,29 @@ void print_color_menu(void)
 	printf("%c%d%c", 186, 8, 179);
 	printf("\033[47;30m"); printf("W"); printf("\033[49;39m");//8 white on black
 	printf("%c\n%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c\n", 186, 200, 205, 207, 205, 202, 205, 207, 205, 202, 205, 207, 205, 202, 205, 207, 205, 188);
+    CURSOR_DOWN_LINE_START(1);
 }
 
 #else
 
 void print_symbol_menu() {
+    CURSOR_UP(5);
     puts("╔═╤═╦═╤═╦═╤═╦═╤═╦═╤═╗");
     puts("║1│░║2│▒║3│▓║4│█║5│▄║");
     puts("╠═╪═╬═╪═╬═╪═╬═╪═╬═╪═╣");
     puts("║6│▌║7│▐║8│▀║9│■║ │ ║");
     puts("╚═╧═╩═╧═╩═╧═╩═╧═╩═╧═╝");
+    CURSOR_DOWN_LINE_START(1);
 }
 
 void print_color_menu() {
+    CURSOR_UP(5);
     puts("╔═╤═╦═╤═╦═╤═╦═╤═╗");
     puts("║1│\x1b[40;37mB\x1b[49;39m║2│\033[49;31m█\033[49;39m║3│\033[49;32m█\033[49;39m║4│\033[49;33m█\033[49;39m║");
     puts("╠═╪═╬═╪═╬═╪═╬═╪═╣");
     puts("║5│\033[49;34m█\033[49;39m║6│\033[49;35m█\033[49;39m║7│\033[49;36m█\033[49;39m║8│\033[47;30mW\033[49;39m║");
     puts("╚═╧═╩═╧═╩═╧═╩═╧═╝");
+    CURSOR_DOWN_LINE_START(1);
 }
 
 #endif
